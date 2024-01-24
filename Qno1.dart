86f4d1b6732f4b275377d9293696a975c750fd24
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-01-24 15:45:10 +0500

    Question no 4 file created

---
+++ Qno1.dart
@@ -0,0 +1,5 @@
+//Question no 1
+void main(){
+  List names = ["taha", "mehmood", "ahsan", "shayan", "ali","akber"];
+  print (names);
+}
--- assignment 1.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  //PRACTICE FOR LOOP
-    List students = ["ali", "hamza", "taha", "mehmood", "arshad"];
-   for (var i = 0; i < students.length; i+=2) {
-     print (students[i]);
-   }
-}
