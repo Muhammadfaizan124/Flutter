14bcc660c5c5a747d5743b9f895faa9beebed989
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-01-25 19:33:59 +0500

    qno 13 done

--- Qno12.dart
+++ Qno12.dart
@@ -1,3 +1,4 @@
+//Question no 12
 void main(){
   List AutomobileCompanies = ["toyota", "nissan", "suzuki", "honda", "mazda", "daihatsu", "changan", "porsche",];
   print (AutomobileCompanies.reversed.toList());
---
+++ Qno13.dart
@@ -0,0 +1,6 @@
+//Question no 13
+void main(){
+  List tableSizesInInches = [12,25,454,77,12,89,89,454,77,12,90,25];
+  Set NewListOfTables = tableSizesInInches.toSet();
+  print (NewListOfTables.toList());
+}
