d0814903bedd8b379a56547819cea6361b850753
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-01-25 19:16:19 +0500

    file created for qno 9 and qno 10 done

---
+++ Qno10.dart
@@ -0,0 +1,6 @@
+//Question no 10
+void main(){
+  List cities = ["karachi","karachi", "dadu", "rawalpindi", "faisalabad", "islamabad", "hyderabad", "dadu", "lahore", "faisalabad", "rawalpindi"];
+  Set uniquecities = cities.toSet();
+  print (uniquecities.toList());
+}
