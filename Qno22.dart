f11095da42146e338d19561c36904b4d0fa382af
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-01-26 16:38:42 +0500

    qno 22 done

---
+++ Qno22.dart
@@ -0,0 +1,11 @@
+//Question no 22
+void main(){
+  Map ShoppingCart = {"product":"Mango","quantity": 50};
+
+  if(ShoppingCart["product"]=="Apple"){
+    print ("Product Found");
+  }
+  else{
+    print ("Product not found");
+  }
+}
