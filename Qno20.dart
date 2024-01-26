032fa9355d562d06c04381ccd8996dcfe0c14e86
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-01-26 16:17:51 +0500

    qno 20 done

---
+++ Qno20.dart
@@ -0,0 +1,11 @@
+//Question no 20
+void main(){
+  Map Car = {"brand": "toyota", "color": "Red", "isSedan": true};
+
+  if(Car["color"]=="Red" && Car["isSedan"]==true){
+    print ("Match");
+  }
+  else{
+    print ("Not Match");
+  }
+}
