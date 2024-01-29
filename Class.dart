e2ed158db44890cdd80a363610dfc009f474d022
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-01-30 00:26:04 +0500

    just practice

--- Class.dart
+++ Class.dart
@@ -1,4 +1,18 @@
 
 void main(){
-  print ("Muhammad faizan");
+  List numbers = [12,-90,67,-66,-38,-12,44];
+  int Pcount = 0;
+  int Ncount = 0;
+  for (int i in numbers) {
+    if(i>0){
+      Pcount++;
+    }
+    else{
+      Ncount++;
+    }
+    
+  }
+  print ("the lenght of list is ${numbers.length}");
+  print ("and there are $Pcount positive numbers");
+  print ("and $Ncount negative numbers");
 }
---
+++ practice.dart
@@ -0,0 +1,10 @@
+void main(){
+  table(4);
+}
+
+table(int n){
+  for (var i = 1; i < 11; i++) {
+    num multiplication = n*i;
+  print ('$n times $i is $multiplication');
+  }
+}
