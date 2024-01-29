6a994bf6ca085ffbdef74d4d52792a29200936a6
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-01-30 01:07:00 +0500

    qno 17 done

--- Qno17.dart
+++ Qno17.dart
@@ -1,27 +1,7 @@
-// //Question no 17
-// void main(){
-//    List originalList = [90,56,71,29,33,52,40,67,87];
-// square(int value) {
-//     return value * value;
-//   }
-//   List<int> squaredList = originalList.map(square).toList();
+//Question no 17
 
-
-//   // // Step 1: Original List
-//   // List<int> originalList = [1, 2, 3, 4, 5];
-
-//   // // Step 2: Transformation Function
-//   // //  square(int value) {
-//   // //   return value * value;
-//   // // }
-
-//   // // Step 3: Applying map()
-//   // // List<int> squaredList = originalList.map(square).toList();
-
-//   // // Step 4: New List
-//   // print("Original List: $originalList");
-//   // print("Squared List: $squaredList");
-// }
-
-
-
+void main(){
+  List originalList = [1,2,3,4,5,6,7,8,9,10];
+  List newList = originalList.map((value) => value*value).toList();
+  print (newList);
+}
