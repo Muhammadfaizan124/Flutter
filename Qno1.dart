a03a86a76c964ec2acd34e623eeb761f6052c7b2
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-02-06 15:41:34 +0500

    qno1 done

---
+++ Qno1.dart
@@ -0,0 +1,9 @@
+//Question no 1
+void main(){
+  List numbers = [12,34,56,90,32,54,76,98,14,75,50,33];
+  for (var i = 0; i < numbers.length; i++) {
+    if(numbers[i]%2==0){
+      print (numbers[i]);
+    }
+  }
+}
