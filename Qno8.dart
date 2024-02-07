672827ac241cf4296247ffc40bd2118b0fcbf9e8
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-02-08 00:32:03 +0500

    qno8 done

---
+++ Qno8.dart
@@ -0,0 +1,15 @@
+//Question no 8
+void main(){
+  var name = "radar";
+  var newname = "";
+  for (var i = name.length-1; i >= 0; i--) {
+     newname += name[i];
+  }
+  
+  if(newname==name){
+      print ("word is palindrome");
+    }
+    else{
+      print ("word is not palindrome");
+    }
+}
