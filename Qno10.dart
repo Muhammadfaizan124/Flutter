d1f1cf2580f7fd20bbf2394a196c2f40dfced306
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-02-08 08:26:46 +0500

    issue facing in output

---
+++ Qno10.dart
@@ -0,0 +1,10 @@
+//Question no 11
+void main() {
+  for (var i = 1; i <= 5; i++) {
+    for (var x = 1; x <= i; x++) {
+      print("*");
+    }
+    print(""); // Print a newline after each row
+  }
+}
+
