5109c57903b95e6febc406bee47a4504cc549cd3
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-02-14 01:15:32 +0500

    qno23 done

--- Qno23.dart
+++ Qno23.dart
@@ -6,6 +6,39 @@ void main(){
  {'name': 'Ryan', 'marks': [70, 65, 75], 'section': 'A', 'rollNumber': 103},
 ];
 
+      var grades = "";
 
+    for (var student in studentDetails) {
+      
+      //loop for calculation marks
+      double sum = 0;
+      double totalmarks = 0;
+      int count = 0;
+     for (var number in student["marks"]) {
+        sum += number;
+        count++;
+        totalmarks = sum / count;
+     }
+      //calculating grades
+      if(totalmarks>=90){
+        grades = "A+";
+      }
+      else if(totalmarks>=80){
+        grades = "A";
+      }
+      else if(totalmarks>=70){
+        grades = "B";
+      }
+      else if(totalmarks>=60){
+        grades = "C";
+      }
+      else if(totalmarks>=50){
+        grades = "D";
+      }
+      else{
+        grades = "fail";
+      }
+    print ("Name: ${student["name"]}, Rollnumber: ${student["rollNumber"]}, Section: ${student["section"]}, and your result is $grades.");
+    }
 
 }
